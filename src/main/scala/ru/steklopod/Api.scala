37d4fe1bc20255fce77a62f6e86fd08b63a7311d
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-05-29 14:30:48 +0300

    Added creating of user in API

--- src/main/scala/ru/steklopod/Api.scala
+++ src/main/scala/ru/steklopod/Api.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directive0
 import akka.http.scaladsl.server.Directives._
-import ru.steklopod.entities.{Game, Helper}
+import ru.steklopod.entities.{Game, Helper, Player}
 import ru.steklopod.repositories.{GameRepository, PlayerRepository}
 import spray.json._
 
@@ -14,39 +14,52 @@ trait GameJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
   //TODO
   implicit val fieldFormat = new JsonFormat[Helper] {
     override def read(json: JsValue): Helper = Helper.fromString(json.convertTo[String])
+
     override def write(obj: Helper): JsValue = JsString(obj.toString)
   }
 
   implicit val gameFormat = jsonFormat9(Game.apply)
+  implicit val playerFormat = jsonFormat3(Player.apply)
 }
 
 
-trait Api extends GameJsonSupport /*with WithAuth */{
+trait Api extends GameJsonSupport /*with WithAuth */ {
   val gameRepository: GameRepository
   val playerRepository: PlayerRepository
-  val route =
+
+  val route = {
     pathPrefix("game") {
       path(LongNumber) { id =>
-//        withAuth {
-          get {
-            parameterMap { paramsMap =>
-              onSuccess(gameRepository.getGame(id)) {
-                case Some(game) =>
-                  complete(StatusCodes.OK -> JsObject(game.toJson.asJsObject.fields ++ Map("params" -> paramsMap.toJson)))
-                case None =>
-                  complete(StatusCodes.NotFound)
-              }
+        //        withAuth {
+        get {
+          parameterMap { paramsMap =>
+            onSuccess(gameRepository.getGame(id)) {
+              case Some(game) =>
+                complete(StatusCodes.OK -> JsObject(game.toJson.asJsObject.fields ++ Map("params" -> paramsMap.toJson)))
+              case None =>
+                complete(StatusCodes.NotFound)
             }
           }
         }
-      } ~ post {
-        entity(as[Game]) { game =>
-          complete {
-            gameRepository.createGame(game).map(_ => StatusCodes.OK)
-          }
+        // }
+      }
+    } ~ post {
+      entity(as[Game]) { game =>
+        complete {
+          gameRepository.createGame(game).map(_ => StatusCodes.OK)
         }
       }
-//    }
+    }
+
+    pathPrefix("user") {
+      post {
+        entity(as[Player]) {
+          player =>
+            complete(playerRepository.createPlayer(player).map(_ => StatusCodes.OK))
+        }
+      }
+    }
+  }
 }
 
 
