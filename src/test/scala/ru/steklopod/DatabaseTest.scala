6d24af1a8d338d705abd9417edce00a8e09c7a4a
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-05-29 16:51:57 +0300

    Get existing user from table test.

--- src/test/scala/ru/steklopod/DatabaseTest.scala
+++ src/test/scala/ru/steklopod/DatabaseTest.scala
@@ -2,19 +2,27 @@ package ru.steklopod
 
 import org.scalatest.{FunSuite, Matchers}
 import ru.steklopod.entities.Player
+import ru.steklopod.repositories.GameDb
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 class DatabaseTest extends FunSuite with Matchers {
 
   test("Get existing user from table") {
-    import scala.util.{Failure, Success}
-
     val username = "testName"
+
+    GameDb.init()
+
+    val player = Await.result(Player.findByName(username), 2 second)
+    print("Player from Await: " + player)
+
     Player.findByName(username).onComplete(
       {
-        case Success(value) => println(s"Got the callback, meaning = $value")
+        case Success(value) => println(s"Found player: $value")
         case Failure(e) => println("Ничего не неайдено")
-        case b => println(b)
       })
   }
 
