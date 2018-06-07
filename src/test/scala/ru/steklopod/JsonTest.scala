47935a2381d6a7d7b5fd08839f7dda63fcf26437
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-06-07 19:03:32 +0300

    тест json - 1

--- src/test/scala/ru/steklopod/JsonTest.scala
+++ src/test/scala/ru/steklopod/JsonTest.scala
@@ -1,37 +1,42 @@
 package ru.steklopod
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import org.scalatest.FunSuite
 import ru.steklopod.entities.{Game, Helper}
 import spray.json.{DefaultJsonProtocol, _}
 
-trait MyJsonProtocol extends DefaultJsonProtocol  with SprayJsonSupport with NullOptions{
+trait MyJsonProtocol extends DefaultJsonProtocol with NullOptions {
 
-  implicit val gameFormat = jsonFormat9(Game.apply)
+  //  implicit val gameFormat = jsonFormat9(Game.apply)
 
-  /* TODO - не рабочий
-    implicit val gameFormat = new JsonWriter[Game] {
-      def write(g: Game): JsValue = {
-        JsObject(
-          "id" -> LongJsonFormat(g.id),
-          "next_step" -> IntJsonFormat(g.nextStep),
-          "won" -> IntJsonFormat(g.won),
-          "finished" -> JsBoolean(g.finished),
-          "players" -> JsString(g.players),
-          "steps" -> IntJsonFormat(g.steps),
-          "size" -> JsString(g.size),
-          "crosses_length_to_win" -> IntJsonFormat(g.crossesLengthToWin),
-          "field" -> JsString(g.fieldPlay)
-        )
+  implicit val gameFormat = new JsonWriter[Game] {
+    def write(g: Game): JsValue = {
+      val id: JsValue = g.id match {
+        case Some(x) => JsNumber(x)
+        case None => JsNull
       }
+      val wonz: JsValue = g.won match {
+        case Some(x) => JsNumber(x)
+        case None => JsNull
+      }
+      JsObject(
+        "id" ->  id,
+        "next_step" -> JsNumber(g.nextStep),
+        "won" -> wonz,
+        "finished" -> JsBoolean(g.finished),
+        "players" -> JsString(g.players),
+        "steps" -> JsNumber(g.steps),
+        "size" -> JsString(g.size),
+        "crosses_length_to_win" -> JsNumber(g.crossesLengthToWin),
+        "field" -> JsString(g.fieldPlay)
+      )
     }
-    */
+  }
 }
 
-class JsonTest extends FunSuite with MyJsonProtocol{
+class JsonTest extends FunSuite with MyJsonProtocol {
 
   test("JSON") {
-    val game = new Game( 1, None, false, "1, 2", 0, Helper.ThreeByThree.toString, 3, "0, 0, 0, 0, 0, 0, 0, 0, 0")
+    val game = new Game(1, None, false, "1, 2", 0, Helper.ThreeByThree.toString, 3, "0, 0, 0, 0, 0, 0, 0, 0, 0")
     val marshalled = game.toJson
     println(marshalled)
   }
