cd18d98dbb686b6c5d4562952de963ab09cc02e4
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-06-08 10:20:39 +0300

    Added simple test (example of marshalling 1 field JSON)

---
+++ src/test/scala/ru/steklopod/JSON1Field.scala
@@ -0,0 +1,18 @@
+package ru.steklopod
+
+import spray.json._
+
+case class SuperClass(omfg: Option[String])
+trait SupperClassJsonSupport  extends DefaultJsonProtocol {
+  implicit val gameFormat = new JsonWriter[SuperClass] {
+    def write(g: SuperClass): JsValue = {
+      JsObject(
+        "omfg" -> g.omfg.toJson
+      )
+    }
+  }
+}
+object Main extends App with SupperClassJsonSupport {
+  println(SuperClass(Some("1234")).toJson)
+}
+
